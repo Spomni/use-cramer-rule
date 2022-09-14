4a69a0b12074bd4877c0b86090a7e257ea2aab33
spomni <mikhail.snitko@gmail.com>
2022-09-14 20:12:19 +0300

    test: describe test cases

---
+++ test/use-cramer-rule.spec.js
@@ -0,0 +1,11 @@
+describe('useCramerRule():', () => {
+  
+  it('should throw a custom type error if passed param is not a matrix of numbers')
+  it('should throw a custom type error if size of the passed matrix is not N x (N+1)')
+
+  it('should return null if the matrix of system equal to 0')
+
+  it('should solve systems with rank 2')
+  it('should solve systems with rank 3')
+  it('should solve systems with any rank')
+})
