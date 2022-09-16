b12409c1439718fe3bf7a13e92fa713e6240e8ec
spomni <mikhail.snitko@gmail.com>
2022-09-16 09:55:03 +0300

    refactor: comment functions in the use-cramer-rule.js file

--- src/use-cramer-rule.js
+++ src/use-cramer-rule.js
@@ -17,6 +17,18 @@ class NotAugmentedMatrixError extends TypeError {
   }
 }
 
+/**
+ * Extract a matrix of the equations system from the augmented matrix of the one
+ *
+ * @param {array[]} augmented - augmented matrix of the system
+ *
+ * @returns {array[]} - matrix of the system
+ *
+ * Example:
+ *   | a11 a12 a13 b1 |    | a11 a12 a13 |
+ *   | a21 a22 a23 b2 | => | a21 a22 a23 |
+ *   | a31 a32 a33 b3 |    | a31 a32 a33 |
+ */
 function extractSystemMatrix(augmented) {
 
   const matrix = augmented.map((row) => [...row])
@@ -27,10 +39,38 @@ function extractSystemMatrix(augmented) {
   return matrix
 }
 
+/**
+ * Extract constantterms from the augmented matrix of the equations system
+ *
+ * @param {array[]} matrix - augmented matrix of the system
+ *
+ * @returns {number} - array of constant terms
+ *
+ * Example:
+ *   | a11 a12 a13 b1 |
+ *   | a21 a22 a23 b2 | => [b1, b2, b3]
+ *   | a31 a32 a33 b3 |
+ */
 function extractConstantTerms(matrix) {
   return matrix.map((row) => row[row.length - 1])
 }
 
+/**
+ * Replace j-column of the equations system matrix with constant terms
+ *
+ * @param {array[]} matrix - matrix of the equations system
+ * @param {number} j - column number
+ * @param {array} terms - constant terms of the system
+ *
+ * @return {array[]}
+ *
+ * Example:
+ *   j = 1; terms = [b1, b2, b3];
+ *
+ *   | a11 a12 a13 |    | a11 b1 a13 |
+ *   | a21 a22 a23 | => | a21 b2 a23 |
+ *   | a31 a32 a33 |    | a31 b3 a33 |
+ */
 function replaceColumn(matrix, j, terms) {
   return matrix.map((row, i) => {
     const newRow = [...row]
@@ -40,7 +80,13 @@ function replaceColumn(matrix, j, terms) {
 }
 
 /**
- * @returns [x1, x2, ..., xn]
+ * Solve system of linear algebraic equations by Cramer's rule
+ *
+ * @param {array[]} augmentedMatrix - augmented matrix of the system
+ *
+ * @returns {number[]} - [x1, x2, ..., xn]
+ *
+ * @throws {TypeError} - if param is not augmented matrix of the system
  */
 function useCramerRule(augmentedMatrix) {
 
